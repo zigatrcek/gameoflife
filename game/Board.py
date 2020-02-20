6463b093aca5544e579a97706ce7bec3866202eb
JZorko <jan@zorko.net>
2020-02-20 11:08:46 +0100

    Remove iterations in iteration

--- game/Board.py
+++ game/Board.py
@@ -9,14 +9,13 @@ class Board:
         if (board is None):
             self.board = np.random.randint(low = 2, size = (width, heigth))
         else:
-            self.board = board
-        self.current_x, self.current_y = 0, 0  
+            self.board = board  
 
     def neighbours(self, collumn, row):
         neighbour_count = 0
         for current_row in range(row - 1, row + 2):
                 for current_collumn in range(collumn - 1, collumn + 2):
-                    if (current_collumn != collumn and current_row != row):
+                    if (current_collumn != collumn and current_row != row):  # Ne šteje sam sebe
                         neighbour_count += self.board[current_row % self.height][current_collumn % self.width]
         return neighbour_count
 
@@ -26,24 +25,24 @@ class Board:
                 neighbour_count = self.neighbours(collumn, row)
                 if (neighbour_count < 2 or neighbour_count > 3):  # Manj kot 2 ali več kot 3 sosedje -> umre
                     self.new_board[row][collumn] = 0
-                elif (neighbour_count == 3):  # Točno trije sosedje -> oživi
+                elif (neighbour_count == 3):  # Točno trije sosedje -> (o)živi
                     self.new_board[row][collumn] = 1
     
-    def iterate(self, iterations=1, batch_size=1, threads=1):
+    def iterate(self, iterations=1, threads=1):
         self.new_board = self.board.copy()
 
         processes = []
-        for _ in range(threads):
+        for i in range(threads):
             p = multiprocessing.Process(target=self.process_range, args=[])
             processes.append(p)
 
-        for _ in range(iterations):
-            for process in processes:
-                process.start()
-            for process in processes:
-                process.join()
+        for process in processes:
+            process.start()
 
-            self.board = new_board
+        for process in processes:
+            process.join()
+
+        self.board = self.new_board
         
 
     def __str__(self):
