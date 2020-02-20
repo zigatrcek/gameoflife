73664fd2a85490ebf3a4edbe82f95069be3ee180
JZorko <jan@zorko.net>
2020-02-20 13:33:40 +0100

    Lokalne spremenljivke

--- game/Board.py
+++ game/Board.py
@@ -12,28 +12,33 @@ class Board:
 
 
     def neighbours(self, row, column):
-        return int(self.board[(row + 1) % self.height][column]
-                 + self.board[(row - 1) % self.height][column]
-                 + self.board[(row + 1) % self.height][(column + 1) % self.width]
-                 + self.board[(row - 1) % self.height][(column + 1) % self.width]
-                 + self.board[(row + 1) % self.height][(column - 1) % self.width]
-                 + self.board[(row - 1) % self.height][(column - 1) % self.width]
-                 + self.board[row][(column + 1) % self.width]
-                 + self.board[row][(column - 1) % self.width])
+        # Deklaracija lokalnih spremenljivk zaradi hitrejšega dostopa
+        board = self.board
+        height = self.height
+        width = self.width
+        
+        return int(board[(row + 1) % height][column]
+                 + board[(row - 1) % height][column]
+                 + board[(row + 1) % height][(column + 1) % width]
+                 + board[(row - 1) % height][(column + 1) % width]
+                 + board[(row + 1) % height][(column - 1) % width]
+                 + board[(row - 1) % height][(column - 1) % width]
+                 + board[row][(column + 1) % width]
+                 + board[row][(column - 1) % width])
 
     
     def run(self, iterations=1):
-        self.new_board = self.board.copy()
+        new_board = self.board.copy()
 
         for _ in range(iterations):
             for row, column in np.ndindex(self.board.shape):
                 neighbour_count = self.neighbours(row, column)
                 if (neighbour_count < 2 or neighbour_count > 3):  # Manj kot 2 ali več kot 3 sosedje -> umre
-                    self.new_board[row][column] = 0
+                    new_board[row][column] = 0
                 elif (neighbour_count == 3):  # Točno trije sosedje -> (o)živi
-                    self.new_board[row][column] = 1
+                    new_board[row][column] = 1
 
-            self.board = self.new_board
+            self.board = new_board
         
 
     def __str__(self):
