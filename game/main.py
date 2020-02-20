4dd8e64a858e3664be4f96556113899d1ff5fbfa
JZorko <jan@zorko.net>
2020-02-20 18:21:21 +0100

    Cleanup and rename to conventions

---
+++ game/main.py
@@ -0,0 +1,28 @@
+from gameoflife import GameOfLife
+import time
+
+if (__name__ == '__main__'):
+    width = int(input('Width: '))
+    height = int(input('Height: '))
+    iterations = int(input('Iterations: '))
+
+    game = GameOfLife(width, height)
+
+    with open("output.txt","w") as f:
+        f.write(str(height) + '\n')
+        t1 = time.time()
+        for i in range(iterations):
+            f.write(str(game))
+            game.run()
+        t2 = time.time()
+        
+    print('Seconds:', t2 - t1)
+
+
+
+
+
+
+
+
+
