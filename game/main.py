59163ce8c32a8640aebdacd1e57d46e542eeeddd
ziga <ziga.trc@gmail.com>
2020-02-20 22:22:14 +0100

    removed redundant run call at the end of loop

--- game/main.py
+++ game/main.py
@@ -15,7 +15,8 @@ if (__name__ == '__main__'):
         t1 = time.time()
         for i in range(iterations):
             f.write(str(game))
-            game.run()
+            if i != iterations-1:
+                game.run()
         t2 = time.time()
         
     print('Seconds:', t2 - t1)
