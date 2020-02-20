a2f2b3c630ce3c39f994593aff5a225a425fa48e
JZorko <jan@zorko.net>
2020-02-20 12:35:59 +0100

    Hardcoded neighbours

--- game/Board.py
+++ game/Board.py
@@ -11,24 +11,26 @@ class Board:
             self.board = board
 
 
-    def neighbours(self, row, collumn):
-        neighbour_count = 0
-        for current_row in range(row - 1, row + 2):
-                for current_collumn in range(collumn - 1, collumn + 2):
-                    if (current_collumn != collumn and current_row != row):  # Ne šteje sam sebe
-                        neighbour_count += self.board[current_row % self.height][current_collumn % self.width]
-        return neighbour_count
+    def neighbours(self, row, column):
+        return int(self.board[(row + 1) % self.height][column]
+                 + self.board[(row - 1) % self.height][column]
+                 + self.board[(row + 1) % self.height][(column + 1) % self.width]
+                 + self.board[(row - 1) % self.height][(column + 1) % self.width]
+                 + self.board[(row + 1) % self.height][(column - 1) % self.width]
+                 + self.board[(row - 1) % self.height][(column - 1) % self.width]
+                 + self.board[row][(column + 1) % self.width]
+                 + self.board[row][(column - 1) % self.width])
 
     
     def run(self, iterations=1):
         self.new_board = self.board.copy()
 
-        for row, collumn in np.ndindex(self.board.shape):
-            neighbour_count = self.neighbours(row, collumn)
+        for row, column in np.ndindex(self.board.shape):
+            neighbour_count = self.neighbours(row, column)
             if (neighbour_count < 2 or neighbour_count > 3):  # Manj kot 2 ali več kot 3 sosedje -> umre
-                self.new_board[row][collumn] = 0
+                self.new_board[row][column] = 0
             elif (neighbour_count == 3):  # Točno trije sosedje -> (o)živi
-                self.new_board[row][collumn] = 1
+                self.new_board[row][column] = 1
 
         self.board = self.new_board
         
