0ef2603c258c4457819a8b5462241ca102280b1b
ziga <ziga.trc@gmail.com>
2020-02-21 08:26:23 +0100

    modified tests

--- tests/test.py
+++ tests/test.py
@@ -1,16 +1,25 @@
 import unittest
 
+import game
 from game.GameOfLife import GameOfLife
 
-class Test_Board(unittest.TestCase):
+
+class Test_GameOfLife(unittest.TestCase):
+    """
+    Run tests from root with the command:
+        'python3 -m unittest tests.test.Test_GameOfLife'
+    """
 
     def setUp(self):
-        self.zeros = GameOfLife([[0 for x in range(10)] for y in range(10)])
-        self.ones = GameOfLife([[1 for x in range(10)] for y in range(10)])
-        self.random = GameOfLife([[0, 1, 1], [0, 0, 1], [0, 1, 0]])
+        self.zeros = GameOfLife(10, 10, [[0 for x in range(10)] for y in range(10)])
+        self.ones = GameOfLife(10, 10, [[1 for x in range(10)] for y in range(10)])
+        self.random = GameOfLife(3, 3, [[0, 1, 1], [0, 0, 1], [0, 1, 0]])
     
     def test_neighbours(self):
-        self.assertEqual(self.zeros.neighbours(0, 0), 0)
-        self.assertEqual(self.ones.neighbours(3, 0), 8)
-        self.assertEqual(self.random.neighbours(1, 1), 4)
-        
+        result_zeros = self.zeros.cell_neighbours(0, 0)
+        result_ones = self.ones.cell_neighbours(3, 0)
+        result_random = self.random.cell_neighbours(1, 1)
+
+        self.assertEqual(result_zeros, 0)
+        self.assertEqual(result_ones, 8)
+        self.assertEqual(result_random, 4)
